d89b6a9cbfc8868099e4444c27465b0a4970bbd1
Xiao Han <hanx7@student.unimelb.edu.au>
2019-09-25 18:49:22 +1000

    Adding projectiles shooting towards aiming object

---
+++ project2/Assets/Scripts/projectiles/ProjectileShooter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShooter : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public GameObject barrel;
+    public GameObject aimingObject;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefab = Resources.Load("Projectile") as GameObject;
+
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = transform.position;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            Vector3 aimingDirection = (aimingObject.transform.position - barrel.transform.position).normalized;
+            rb.velocity = aimingDirection * speed;
+        }
+    }
+}
