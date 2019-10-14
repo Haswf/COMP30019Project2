e31cf4db125b68c47e33d6b84027aac750ce11a2
Yi <42195839+839224346@users.noreply.github.com>
2019-10-15 00:46:45 +1100

    fixed subcamera

---
+++ project2/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Camera BattleShipCamere;
+
+    public Camera ShellFollowingCamera;
+    public Camera AimingCamera;
+    private ShellFollower _follower;
+    private string CurrantCamera;    // Start is called before the first frame update
+    void Start()
+    {
+        CurrantCamera =  "BattleShipCamera";
+        BattleShipCamere.enabled = true;
+        ShellFollowingCamera.enabled = false;
+        AimingCamera.enabled = false;
+        _follower = ShellFollowingCamera.GetComponent<ShellFollower>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrantCamera.CompareTo("BattleShipCamera") == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.F) && _follower.target)
+            {
+                BattleShipCamere.enabled = !BattleShipCamere.enabled;
+                AimingCamera.enabled = false;
+                ShellFollowingCamera.enabled = !ShellFollowingCamera.enabled;
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                CurrantCamera = "AimingCamera";
+                BattleShipCamere.enabled = false;
+                AimingCamera.enabled = true;
+                ShellFollowingCamera.enabled = false;
+            }
+            if (_follower.target == null)
+            {
+                BattleShipCamere.enabled = true;
+                AimingCamera.enabled = false;
+                ShellFollowingCamera.enabled = false;
+            }
+        }
+        else if (CurrantCamera.CompareTo("AimingCamera") == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.F) && _follower.target)
+            {
+                BattleShipCamere.enabled = false;
+                AimingCamera.enabled = !AimingCamera.enabled;
+                ShellFollowingCamera.enabled = !ShellFollowingCamera.enabled;
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                CurrantCamera = "BattleShipCamera";
+                BattleShipCamere.enabled = true;
+                AimingCamera.enabled = false;
+                ShellFollowingCamera.enabled = false;
+            }
+            if (_follower.target == null)
+            {
+                BattleShipCamere.enabled = false;
+                AimingCamera.enabled = true;
+                ShellFollowingCamera.enabled = false;
+            }
+        }
+    }
+}
---
+++ project2/Assets/Scripts/Camera/SubCameraLock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class SubCameraLock : MonoBehaviour
+{
+
+    float mouseX, mouseY;
+
+
+    private const float Y_ANGLE_MAX = 0.0f;
+    private const float Y_ANGLE_MIN = -60.0f;
+    private const float X_ROTATION_MIN = 50.0f;
+    private const float X_ROTATION_MAX = 79.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X");
+        mouseY -= Input.GetAxis("Mouse Y");
+       // mouseY = Mathf.Clamp(mouseY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+       
+        transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Vector3 currentRotation = transform.localRotation.eulerAngles;
+        currentRotation.x = Mathf.Clamp(currentRotation.x, X_ROTATION_MIN, X_ROTATION_MAX);
+        //transform.localRotation = Quaternion.Euler(currentRotation.x, 0, 0);
+
+    }
+}
