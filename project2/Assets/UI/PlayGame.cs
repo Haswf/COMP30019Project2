8e6906b3f5d829540151c5b1708042ab9331fffe
Xiao Han <hanx7@student.unimelb.edu.au>
2019-10-14 14:59:37 +1100

    add menu scene

---
+++ project2/Assets/UI/PlayGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        print("play");
+        SceneManager.LoadScene(1);
+
+    }
+}
