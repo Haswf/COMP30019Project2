fe7fc358b06b31635d8998532a6360f754aa5772
Xiao Han <hanx7@student.unimelb.edu.au>
2019-10-17 02:28:02 +1100

    adding choose boat page

---
+++ project2/Assets/UI/ItemSelect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSelect : MonoBehaviour
+{
+    public Image ImageSelection;
+    public List<Sprite> ItemList = new List<Sprite>();
+    private int itemSpot = 0;
+   
+    public void RightSelection()
+    {
+        if(itemSpot < ItemList.Count - 1)
+        {
+            itemSpot++;
+            ImageSelection.sprite = ItemList[itemSpot];
+        }
+    }
+
+    public void LeftSelection()
+    {
+        if (itemSpot > 0)
+        {
+            itemSpot--;
+            ImageSelection.sprite = ItemList[itemSpot];
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
