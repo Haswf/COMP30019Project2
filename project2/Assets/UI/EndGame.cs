56ede56485728d312927544e6cfd649ff38668b9
Xiao Han <hanx7@student.unimelb.edu.au>
2019-10-17 19:01:53 +1100

    adding game over scene

---
+++ project2/Assets/UI/EndGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void End()
+    {
+        print("game over");
+        SceneManager.LoadScene(0);
+
+    }
+}
