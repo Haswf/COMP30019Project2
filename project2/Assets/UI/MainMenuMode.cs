b19451e9ec530a80ebc8c14fbf93e92856bbcc8b
Xiao Han <hanx7@student.unimelb.edu.au>
2019-10-24 14:27:03 +1100

    improving  tutorial scene ,detect long press

---
+++ project2/Assets/UI/MainMenuMode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuMode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BackToMainMenu()
+    {
+        print("main");
+        SceneManager.LoadScene(0);
+
+    }
+}
--- project2/Assets/UI/TutorialIndicator.cs
+++ project2/Assets/UI/TutorialIndicator.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TutorialIndicator : MonoBehaviour
-{    
+{
     //public GameObject boat;
     private Text tutorialText;
     //private HealthManager healthMGR;
     private int step;
+    private int timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,44 +22,53 @@ public class TutorialIndicator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //tutorialText.text = "Well done! Press a to rotate the boat";
-        if (step == 0) {
+
+        if (step == 0)
+        {
             if (Input.GetKey(KeyCode.W))
             {
                 print("get key down!!!!!");
-                tutorialText.text = "Well done! Press a to rotate the boat";
+                timer += 1;
+                if(timer == 100)
+                {
+                    tutorialText.text = "Well done! Long press A to rotate the boat";
+                    timer = 0;
+                }
+
             }
             if (Input.GetKey(KeyCode.A))
             {
-                tutorialText.text = "Well done! Now you know how to move the boat!\n Try to move mouse from left to right";
-                step = 1;
+                timer += 1;
+                if(timer == 100)
+                {
+                    tutorialText.text = "Well done! Now you know how to move the boat!\n Try to move mouse ";
+                    step = 1;
+                }
+
             }
         }
 
         if (step == 1)
         {
-            if(Input.GetAxis("Mouse X") != 0)
+            if (Input.GetAxis("Mouse X") != 0 && Input.GetAxis("Mouse Y") != 0)
             {
-                tutorialText.text = "Well done!\n Now you know how to aim horizontally\n Try to move mouse up and down";
-            }
-            if(Input.GetAxis("Mouse Y") != 0)
-            {
-                tutorialText.text = "Well done!\n Now you know how to aim vertically\n Try press X";
+                tutorialText.text = "Well done!\n Now you know how to aim !\n Try to press C and change perspective";
                 step = 2;
             }
+
 
         }
         if (step == 2)
         {
-            if (Input.GetKey(KeyCode.X))
+            if (Input.GetKey(KeyCode.C))
             {
                 tutorialText.text = "Well done! Press Z to change again";
             }
             if (Input.GetKey(KeyCode.Z))
             {
-                tutorialText.text = "Well done! Press C to change again";
+                tutorialText.text = "Well done! Press X to change again";
             }
-            if (Input.GetKey(KeyCode.C))
+            if (Input.GetKey(KeyCode.X))
             {
                 tutorialText.text = "Well done! Now you know how to switch perspectives.\n Try left click your mouse";
                 step = 3;
@@ -65,7 +76,7 @@ public class TutorialIndicator : MonoBehaviour
 
         }
 
-        if(step == 3)
+        if (step == 3)
         {
             if (Input.GetKey(KeyCode.Mouse0))
             {
@@ -73,21 +84,20 @@ public class TutorialIndicator : MonoBehaviour
                 step = 4;
             }
         }
-        if(step == 4)
+        if (step == 4)
         {
-            tutorialText.text = "Tutorial finished!\n Enjoy the game";
+            tutorialText.text = "Tutorial finished!\n Right click your mouse ,go back to Mainmenu and Enjoy the game";
+            step = 5;
+        }
+        if (step == 5)
+        {
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                SceneManager.LoadScene(0);
+            }
         }
     }
 
-    void Movement() {
-        print("in movement!");
-        if (Input.GetKey(KeyCode.W)){
-            print("get key down!!!!!");
-            tutorialText.text = "Well done! Press a to rotate the boat";
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            tutorialText.text = "Well done! Now you know how to move the boat!";
-        }
-    }
+
+
 }
