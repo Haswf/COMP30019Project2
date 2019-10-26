f93d2e89e14a3f77ae3b2a611089d8d5b80f7f16
Xiao Han <hanx7@student.unimelb.edu.au>
2019-10-26 22:26:51 +1100

    fixing locked mouse, add three upgrade buttons

---
+++ project2/Assets/UI/unlockMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unlockMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // _battleshipCamera = GetComponent<Camera>();
+        // make cursor invisible
+        Cursor.visible = true;
+        // lock cursor at the center of the screen
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
